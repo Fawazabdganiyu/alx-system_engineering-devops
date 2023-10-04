23ea7dbe8aaecc7d88a55a0f71d058b7982b4501
fawazabdganiyu <fawazabdganiyu@gmail>
2023-10-04 03:45:55 +0100

    Refactor code

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/env ruby
-
-match = ARGV[0].match(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/)
-if match
-  sender = match[1]
-  receiver = match[2]
-  flags = match[3]
-end
-
-puts "#{sender},#{receiver},#{flags}"
+puts ARGV[0].scan(/\[from:(.*?)\] \[to:(.*?)\] \[flags:(.*?)\]/).join(",")
