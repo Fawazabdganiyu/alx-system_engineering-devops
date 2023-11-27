7b94c421187a7ea7247718bcda37e59860f6d5f4
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-27 08:08:55 +0100

    Modify `/etc/ssh/ssh_config`

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Manifest to manage SSH configuration using file_line
 file_line { 'Turn off passwd auth':
   ensure => present,
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
 
 file_line { 'Declare identity file':
   ensure => present,
-  path   => '/root/.ssh/config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
   }
