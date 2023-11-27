d8d347b2984ec96262288b4fc76df7fff7bcfe7c
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-27 07:46:54 +0100

    Use augeas for manifest

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,13 @@
-# Set up my client SSH configuration file
-file_line { 'ssh_config':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    IdentityFile ~/.ssh/school',
-}
-
-file_line { 'ssh_config':
-  ensure => present,
-  path   => '/etc/ssh/ssh_config',
-  line   => '    PasswordAuthentication no',
+# Manifest to manage SSH configuration using augeas
+augeas { 'ssh_config':
+  incl    => '/etc/ssh/ssh_config',
+  lens    => 'Ssh.lns',
+  context => '/etc/ssh/ssh_config',
+  changes => [
+    'set Host 54.197.78.222',
+    'set Host[.="54.197.78.222"]/HostName 54.197.78.222',
+    'set Host[.="54.197.78.222"]/User ubuntu',
+    'set Host[.="54.197.78.222"]/IdentityFile ~/.ssh/school',
+    'set Host[.="54.197.78.222"]/PasswordAuthentication no',
+  ],
 }
