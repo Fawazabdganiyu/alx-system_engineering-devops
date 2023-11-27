a075d4dec0000f8322a309ef5236c1a439e2ca90
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-27 06:56:35 +0100

    test

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Set up my client SSH configuration file
 file_line { 'ssh_config':
   ensure => present,
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    IdentityFile ~/.ssh/school',
 }
 
 file_line { 'ssh_config':
   ensure => present,
-  path   => '/etc/ssh/sshd_config',
+  path   => '/etc/ssh/ssh_config',
   line   => '    PasswordAuthentication no',
 }
