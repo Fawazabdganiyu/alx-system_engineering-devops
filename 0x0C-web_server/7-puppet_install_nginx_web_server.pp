f7815ff9f306fd35a6066fc87bb23bbb04619853
fawazabdganiyu <fawazabdganiyu@gmail>
2023-11-29 04:18:17 +0100

    Fix syntax errors

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,7 +1,7 @@
 # A manifest for nginx web server configuration
 
 exec { 'apt_update':
-  command => 'apt-get update'
+  command => 'apt-get update',
   path    => ['/usr/bin', '/usr/sbin'],
 }
 
@@ -21,8 +21,8 @@ $redirect_rule = '\n\tlocation /redirect_me {\n\
         \treturn 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;\n\
         }'
 exec { 'redirect_me':
-  command => "sudo sed -i '/server_name _;/a \ ${redirect_rule}' /etc/nginx/sites-available/default"
-  path    => '/usr/bin:/bin'
+  command => "sudo sed -i '/server_name _;/a \\ ${redirect_rule}' /etc/nginx/sites-available/default",
+  path    => '/usr/bin:/bin',
 }
 
 service { 'nginx':
