7f2ec957308ab6b8694cf28439e09cc48633c76b
fawazabdganiyu <fawazabdganiyu@gmail>
2023-12-05 04:14:36 +0100

    Change the `exec` to `file_line`

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,22 @@
 # Install and configure `Nginx` web server to have a custome header
 exec { 'apt_update':
   command => 'apt-get update',
-  path    => '/usr/bin:/usr/sbin'
+  path    => '/usr/bin:/usr/sbin',
 }
 
 package { 'nginx':
   ensure  => installed,
-  require => Exec['apt_update']
+  require => Exec['apt_update'],
 }
 
-exec { 'custom_header':
-  command => "sudo sed -i '/server_name _;/a \\ \n\tadd_header X-Served-By \"${hostname}\";' /etc/nginx/sites-available/default",
-  path    => '/usr/bin:bin'
+file_line { 'add_custom_header':
+  path  => '/etc/nginx/sites-available/default',
+  line  => "\n\tadd_header X-Served-By ${hostname};",
+  after => 'server_name _;',
 }
 
 service { 'nginx':
   ensure  => running,
   enable  => true,
-  require => Package['nginx']
+  require => Package['nginx'],
 }
