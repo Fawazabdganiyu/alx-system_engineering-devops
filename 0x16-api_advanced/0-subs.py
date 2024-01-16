c4390c3aa92a1db929cd86db91f65abc50e1c3ae
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-16 23:23:34 +0100

    doc: rewrite the module documentation

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,15 +1,17 @@
 #!/usr/bin/python3
-""" Query the Reddit API and returns the number of subscribers
+"""
+Query the Reddit API and returns the number of subscribers
 """
 
 
 def number_of_subscribers(subreddit):
-    """ Get the numbers of subscribers in the given subreddit
+    """
+    Get the numbers of subscribers in the given subreddit
     """
     import requests
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'My User Agent 1.0'}
+    headers = {'User-Agent': 'My User Agent/1.0'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 200:
