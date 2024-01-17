843c27c6bfdd0decb7ccaa5699d596b4e309864d
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-17 05:55:20 +0100

    fix: include after explicitly in the recursive call

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,13 +6,13 @@ for a given subreddit
 import requests
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=None):
     """
     Get the list containing the titles of all hot articles of a subreddit
     """
     url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
     headers = {'User-Agent': 'My Custom Agent/1.0'}
-    params = {'limit': 100}
+    params = {'limit': 100, 'after': after}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
     if response.status_code != 200:
@@ -21,8 +21,7 @@ def recurse(subreddit, hot_list=[]):
     for post in posts:
         hot_list.append(post.get('data').get('title'))
     after = response.json().get('data').get('after')
-    if after:
-        params['after'] = after
-        recurse(subreddit, hot_list)
+    if after is None:
+        return hot_list
 
-    return hot_list
+    return recurse(subreddit, hot_list, after)
