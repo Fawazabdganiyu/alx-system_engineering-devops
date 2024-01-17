e6f34b0af6fbfa08d3fed151c915a8a4adfbb962
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-17 05:43:53 +0100

    Get the hot posts for a subreddit recursively

---
+++ 0x16-api_advanced/100-count.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#!/usr/bin/python3
+"""
+Query the Reddit API, parse the the title of all hot articles,
+and print a sorted count of given keywords (case-insensitive,
+delimited by spaces. Javascript should count as javascript,
+but java should not).
+"""
+import requests
+
+
+def count_words(subreddit, word_list):
+    """ Count words """
---
+++ 0x16-api_advanced/2-recurse.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Query the Reddit API and return a list containing the titles of all hot articles
+for a given subreddit
+"""
+import requests
+
+
+def recurse(subreddit, hot_list=[]):
+    """
+    Get the list containing the titles of all hot articles of a subreddit
+    """
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    headers = {'User-Agent': 'My Custom Agent/1.0'}
+    params = {'limit': 100}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+    if response.status_code != 200:
+        return None
+    posts = response.json().get('data').get('children')
+    for post in posts:
+        hot_list.append(post.get('data').get('title'))
+    after = response.json().get('data').get('after')
+    if after:
+        params['after'] = after
+        recurse(subreddit, hot_list)
+
+    return hot_list
