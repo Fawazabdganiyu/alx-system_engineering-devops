d652805cc17c00e008c47243aaebb9a7775d8b6b
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-18 16:27:11 +0100

    Fix python coding style

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,7 +13,7 @@ def top_ten(subreddit):
     headers = {'User-Agent': 'My Custom Agent/1.0'}
     params = {'limit': 10}
     r = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
+                     allow_redirects=False)
     if r.status_code != 200:
         print(None)
         return
