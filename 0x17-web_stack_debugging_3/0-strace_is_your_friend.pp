7e09af122922361ecaee567f3a2c0468a84b423c
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-18 09:06:13 +0100

    Fix the typo in wordpress codebase

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,1 +1,5 @@
-# Strace is your friend
+# Fix the typo in /var/www/html/wp-settings.php
+exec { 'fix-wp-settings-typo':
+  command => 'sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  path    => ['/usr/bin', '/bin'],
+}
