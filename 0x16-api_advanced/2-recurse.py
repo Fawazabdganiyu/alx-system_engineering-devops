9c29207d2839a098c8d6729fb0378b1f9732f867
fawazabdganiyu <fawazabdganiyu@gmail.com>
2024-01-18 16:25:37 +0100

    Fix python coding style

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """
-Query the Reddit API and return a list containing the titles of all hot articles
-for a given subreddit
+Query the Reddit API and return a list containing the titles of all
+hot articles for a given subreddit
 """
 import requests
 
